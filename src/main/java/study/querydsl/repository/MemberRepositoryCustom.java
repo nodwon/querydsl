1ecf65a72ea36f441198f692a443ba89c0eb5d15
nodwon <nodwon5024@gmail.com>
2023-06-11 16:32:57 +0900

    페이징 처리

--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -1,14 +1,15 @@
 package study.querydsl.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
-    List<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
-    List<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 
 }
